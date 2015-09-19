6ff20ade7ab81a5e04e30a005b0fee5b7ab4189a
Igor Kapkov <igasgeek@me.com>
2015-09-19 12:39:34 +1000

    Add layers to stuff for update

--- functions/bill.fish
+++ functions/bill.fish
@@ -1,6 +1,9 @@
 function bill
   echo "Updating Fish"
   cd ~/.config/fish; and git pull ; and cd -
-  echo "Updating Spacemacs"
+  echo "Updating Spacemacs and layers"
   cd ~/.emacs.d; and git pull ; and cd -
+  cd ~/.emacs.d/private/igas; and git pull ; and cd -
+  cd ~/.emacs.d/private/ruby-min; and git pull ; and cd -
+  cd ~/.emacs.d/private/ruby-on-rails-min; and git pull ; and cd -
 end
