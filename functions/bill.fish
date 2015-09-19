43c68a248c1d1c2c7428d17436b804c107e280e7
Igor Kapkov <igasgeek@me.com>
2015-09-19 12:58:55 +1000

    Add highlighting

--- functions/bill.fish
+++ functions/bill.fish
@@ -1,7 +1,12 @@
 function bill
+  set_color red
   echo "Updating Fish"
+  set_color normal
   cd ~/.config/fish; and git pull ; and cd -
+  echo
+  set_color red
   echo "Updating Spacemacs and layers"
+  set_color normal
   cd ~/.emacs.d; and git pull ; and cd -
   cd ~/.emacs.d/private/igas; and git pull ; and cd -
   cd ~/.emacs.d/private/ruby-min; and git pull ; and cd -
