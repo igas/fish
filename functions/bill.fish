500056044e1801c5793e9592c690e61f2438c770
Igor Kapkov <igasgeek@me.com>
2015-10-23 10:38:37 +1100

    remove ruby layers forks from update

--- functions/bill.fish
+++ functions/bill.fish
@@ -9,8 +9,6 @@ function bill
   set_color normal
   cd ~/.emacs.d; and git pull ; and cd -
   cd ~/.emacs.d/private/igas; and git pull ; and cd -
-  cd ~/.emacs.d/private/ruby-min; and git pull ; and cd -
-  cd ~/.emacs.d/private/ruby-on-rails-min; and git pull ; and cd -
   echo
   set_color red
   echo "Updating dotfiles"
