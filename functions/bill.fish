541fbfc03ef1c3421c9f8bd924ed2651ef57c270
Igor Kapkov <igasgeek@me.com>
2016-05-02 12:13:19 +1000

    update bill function

--- functions/bill.fish
+++ functions/bill.fish
@@ -2,16 +2,12 @@ function bill
   set_color red
   echo "Updating Fish"
   set_color normal
-  cd ~/.config/fish; and git pull ; and cd -
-  echo
-  set_color red
-  echo "Updating Spacemacs and layers"
-  set_color normal
-  cd ~/.emacs.d; and git pull ; and cd -
-  cd ~/.emacs.d/private/igas; and git pull ; and cd -
+  pushd ~/.config/fish; and git pull
+  popd
   echo
   set_color red
   echo "Updating dotfiles"
   set_color normal
-  cd (dirname (readlink ~/.spacemacs)); and git pull ; and cd -
+  pushd (dirname (readlink ~/.spacemacs)); and git pull
+  popd
 end
